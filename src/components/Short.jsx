6e8f87750292502e2fbc2d96dff4a130ace3430a
stefan <stefan@gmail.com>
2024-12-27 01:21:32 +0100

    add code to short

--- src/components/Short.jsx
+++ src/components/Short.jsx
@@ -1,6 +1,17 @@
 function Short() {
-    return <div></div>;
-  }
-  
-  export default Short;
-  
+  return (
+    <>
+      <div className="w-full bg-white relative items-center justify-between px-6 py-4 flex rounded-md">
+        <p>https://www.frontendmentor.io</p>
+        <div className="flex items-center gap-5 text-neutral-veryDarkBlue">
+          <p className="text-primary-cyan">https://reLink/k4IKyk</p>
+          <button className="bg-primary-cyan font-bold text-white rounded-md py-2 px-8">
+            Copy
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Short;
--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -2,23 +2,25 @@ import Short from "./Short";
 
 function Shorten() {
   return (
-    <section className="mx-36 relative">
-      <div className="w-full absolute -top-[68px]">
-        <div className="bg-primary-violet rounded-xl">
-          <form className="lg:bg-bg_shorten_desktop bg-bg_shorten_mobile rounded-xl flex justify-center items-center p-10 gap-5 bg-cover">
-            <input
-              className="py-4 px-6 rounded-xl w-[85%]"
-              type="text"
-              placeholder="Shorten a link here..."
-            />
-            <button className="text-white font-bold bg-primary-cyan py-4 rounded-xl w-[15%] ">
-              Shorten It!
-            </button>
-          </form>
-          <p className="text-secondary-red hidden">Please add a link</p>
+    <section className="px-36 bg-slate-100">
+      <div className="relative">
+        <div className="w-full absolute -top-[68px]">
+          <div className="bg-primary-violet rounded-xl">
+            <form className="lg:bg-bg_shorten_desktop bg-bg_shorten_mobile rounded-xl flex justify-center items-center p-10 gap-5 bg-cover">
+              <input
+                className="py-4 px-6 rounded-xl w-[85%]"
+                type="text"
+                placeholder="Shorten a link here..."
+              />
+              <button className="text-white font-bold bg-primary-cyan py-4 rounded-xl w-[15%] ">
+                Shorten It!
+              </button>
+            </form>
+            <p className="text-secondary-red hidden">Please add a link</p>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="gap-y-4 flex pt-24 flex-col">
         <Short />
       </div>
     </section>
