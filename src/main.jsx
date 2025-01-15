47185415d20284cca3b40a9b788c529fbcf0b834
marko <marko@gmail.com>
2025-01-15 22:30:36 +0100

    aos

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AOS from "aos";
 import "aos/dist/aos.css";
-
-// Initialize AOS
-AOS.init({
-  duration: 300,
-  disable: function () {
-    return window.innerWidth >= 768;
-  },
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
